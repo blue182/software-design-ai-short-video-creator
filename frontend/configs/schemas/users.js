34597e8200014997be5e71bb365d46f80e4a2925
blue182 <nguyentruc14150107@gmail.com>
2025-06-26 14:54:42 +0700

    function sign in, sign up

--- frontend/configs/schemas/users.js
+++ frontend/configs/schemas/users.js
@@ -3,10 +3,9 @@ import { pgTable, serial, varchar, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
-    username: varchar('username', { length: 50 }).unique().notNull(),
-    fullname: varchar('fullname', { length: 50 }),
+    username: varchar('username', { length: 50 }),
+    profile_text: varchar('profile_text', { length: 1000 }),
     email: varchar('email', { length: 100 }).unique().notNull(),
-    passwordHash: text('password_hash').notNull(),
     avatarUrl: text('avatar_url'),
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at').defaultNow(),
